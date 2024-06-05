444d0df7a36683de6c5d8e3650cbf672a2f1e6cf
Jun-Min Lee <ljm56897@gmail.com>
2024-06-05 15:20:29 +0900

    feat: server argparser로 변환

--- demo/server.py
+++ demo/server.py
@@ -4,26 +4,34 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), 'src'))
 
 import uvicorn
+import argparse
 from sconf import Config
 from fastapi import FastAPI, WebSocket
 
 import torch
 
 from tools import Chatter
+from utils.training_utils import choose_proper_resume_model
 
 
 
 app = FastAPI()
 
-model_dir = 'outputs/phi3/mras_en_filtered_simplified6'
-template_path = '/home/junmin/Documents/Python/llm/templates/phi3_templates/template1.json'
-config = Config(os.path.join(model_dir, 'args.yaml'))
-model_path = os.path.join(model_dir, 'weights/model_epoch:1_step:31_last_best.pt')
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-is_greedy = True
-save_context = False
-efficient_load = False
-chatter = Chatter(config, model_path, device, template_path, save_context, efficient_load, is_greedy)
+# argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-m', '--model_dir', type=str, required=True)
+parser.add_argument('-l', '--load_model_type', type=str, default='metric', required=False, choices=['metric', 'loss', 'last'])
+parser.add_argument('-t', '--template_path', type=str, required=True)
+parser.add_argument('-d', '--device', type=str, default='0', required=False)
+parser.add_argument('--is_greedy', action='store_true', required=False)
+parser.add_argument('--efficient_load', action='store_true', required=False)
+parser.add_argument('--save_context', action='store_true', required=False)
+args = parser.parse_args()
+
+config = Config(os.path.join(args.model_dir, 'args.yaml'))
+model_path = choose_proper_resume_model(args.model_dir, args.load_model_type)
+device = torch.device('cpu') if args.device == 'cpu' else torch.device(f'cuda:{args.device}')
+chatter = Chatter(config, model_path, device, args.template_path, args.save_context, args.efficient_load, args.is_greedy)
 
 
 
