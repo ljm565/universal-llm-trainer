e7156c85336eab2c426b69d47a24864f45598dd6
Jun-Min Lee <ljm56897@gmail.com>
2024-09-20 10:20:16 +0900

    feat: Add sanity check for attn_implementation

--- src/utils/func_utils.py
+++ src/utils/func_utils.py
@@ -14,12 +14,15 @@ from utils import (
 
 def sanity_check(clazz):
     assert clazz.optimizer_step_criterion in OPTIM_CRITERION, \
-        OPTIM_CRITERION_MSG + f' but got {colorstr(clazz.optimizer_step_criterion)}'
+        OPTIM_CRITERION_MSG + f' but got {colorstr(clazz.optimizer_step_criterion)}.'
     assert clazz.scheduler_type in SCHEDULER_TYPE, \
-        SCHEDULER_MSG + f' but got {colorstr(clazz.scheduler_type)}'
+        SCHEDULER_MSG + f' but got {colorstr(clazz.scheduler_type)}.'
     if clazz.config.fsdp_train:
         assert clazz.config.fsdp_hyperparameters.wrap_policy in FSDP_WRAP_TYPE, \
-            FSDP_WRAP_MSG + f' but got {colorstr(clazz.config.fsdp_hyperparameters.wrap_policy)}'
+            FSDP_WRAP_MSG + f' but got {colorstr(clazz.config.fsdp_hyperparameters.wrap_policy)}.'
+
+    if clazz.config.attn_implementation:
+        assert clazz.config.amp_training, colorstr('You must set amp_training option to True if you use attn_implementation option.')
         
     
 def select_training_type(train_type):
