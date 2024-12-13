be6d584a8422fa4d254591121dbab13ea104b7e6
Jun-Min Lee <ljm56897@gmail.com>
2024-12-13 13:45:30 +0900

    fix: Loro train bug

--- src/trainer/loro_trainer.py
+++ src/trainer/loro_trainer.py
@@ -48,6 +48,7 @@ class LoRoTrainer:
         self.scheduler_type = self.config.scheduler_type
         self.metrics = self.config.metrics
         if self.is_training_mode:
+            config.is_training_mode = True
             self.save_dir = make_project_dir(self.config, self.is_rank_zero)
             self.wdir = self.save_dir / 'weights'
         else:
