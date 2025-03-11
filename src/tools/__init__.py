e0840ae0c6041c5fe4f99c401da5067d95be88af
Jun-Min Lee <ljm56897@gmail.com>
2025-03-11 08:14:02 +0000

    feat: Delete langtranslator class

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -3,5 +3,4 @@ from .evaluator import Evaluator
 from .model_manager import ModelManager
 from .early_stopper import EarlyStopper
 from .training_logger import TrainingLogger
-from .lang_translator import LangTranslator
 from .chatter import Chatter
