a419495a0790058c3e9a7c51d22f7df2a6944c01
Jun-Min Lee <ljm56897@gmail.com>
2025-03-12 02:21:28 +0000

    feat: Update version 1.5.0 -> 1.5.1

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -10,7 +10,7 @@ from tqdm import tqdm as tqdm_original
 import torch
 
 
-LOGGING_NAME = 'LLM_TRAINER_1.5.0'
+LOGGING_NAME = 'LLM_TRAINER_1.5.1'
 VERBOSE = True
 RANK = int(os.getenv('RANK', -1))
 MACOS, LINUX, WINDOWS = (platform.system() == x for x in ['Darwin', 'Linux', 'Windows'])  # environment booleans
