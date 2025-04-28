1071104da6258ac4fbbdaf91fd0ef120f0b560b2
Jun-Min Lee <ljm56897@gmail.com>
2025-04-28 12:58:29 +0900

    feat: Update eleuther evaluation codes

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,5 +1,6 @@
 from .ema import ModelEMA
 from .evaluator import Evaluator
+from .eleuther_evaluator import EleutherEvaluator
 from .model_manager import ModelManager
 from .early_stopper import EarlyStopper
 from .training_logger import TrainingLogger
